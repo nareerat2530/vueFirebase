8cd3ad7f604ea5e2fa2a84d089e4fdcc4d1dfa0b
Nareerat Srisai <66684500+nareerat2530@users.noreply.github.com>
2022-06-14 15:50:25 +0200

    fix homepage

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,18 +1,31 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="#">Home</b-navbar-brand>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand href="#">My site</b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item href="#">Link</b-nav-item>
-          <b-nav-item href="#" disabled>Disabled</b-nav-item>
+          <b-nav-item
+            ><nuxt-link class="navbar-item" to="/admin"
+              >Admin</nuxt-link
+            ></b-nav-item
+          >
+        </b-navbar-nav>
+        <b-navbar-nav>
+          <b-nav-item
+            ><nuxt-link class="navbar-item" to="/login"
+              >login</nuxt-link
+            ></b-nav-item
+          >
+        </b-navbar-nav>
+        <b-navbar-nav>
+          <nuxt-link class="navbar-item" to="/register">Register</nuxt-link>
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
+        <!-- <b-navbar-nav class="ml-auto">
           <b-nav-form>
             <b-form-input
               size="sm"
@@ -23,17 +36,14 @@
               >Search</b-button
             >
           </b-nav-form>
-
-          <b-nav-item-dropdown right>
-            <!-- Using 'button-content' slot -->
-            <template #button-content>
-              <em>User</em>
-            </template>
-            <b-dropdown-item href="#">Profile</b-dropdown-item>
-            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-          </b-nav-item-dropdown>
-        </b-navbar-nav>
+        </b-navbar-nav> -->
       </b-collapse>
     </b-navbar>
   </div>
 </template>
+
+<script>
+export default {}
+</script>
+
+<style lang="" scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,19 @@
 <template lang="">
   <div class="container">
-    <div>
+    
       <NavBar/>
-    </div>
     
-    <button class="button-logout" @click="onLogout">log out</button>
-    <nuxt-link class="button-logout" to="/profile">profile</nuxt-link>
     
-   <div v-if="$auth.loggedIn">
+    <!-- <button class="button-logout" @click="onLogout">log out</button>
+    <nuxt-link class="button-logout" to="/profile">profile</nuxt-link> -->
+    
+   <!-- <div v-if="$auth.loggedIn"> -->
      <h1>Hello {{ decoded.name }}</h1>
-      <span>This is all you have got {{ decoded.email }}</span>
+      <p>This is all you have got {{ decoded.email }}</p>
    </div>
-   <div v-else>
+   <!-- <div v-else>
      <h1>Hello Log in or Register first OK?</h1>
-   </div>
+   </div> -->
       
     </div>
   </div>
